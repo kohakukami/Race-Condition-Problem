1dc01bb3e99e6df6e2b8b44730ef76db9c5d5722
Kohaku <phucpddfx03285@funix.edu.vn>
2026-01-01 15:32:15 +0700

    push to github

---
+++ cFiles/exe1.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+int mynum = 0; /* A global variable*/
+
+int main(void) {
+  pid_t childPID;
+  int i;
+  //create child process
+  childPID = fork();
+
+  // is child process running?
+  if (childPID == 0) // Child process
+  {
+    printf("Child process ID: %d\n", getpid());
+    for (i = 0;; ++i) /*both processes do this*/ {
+      mynum = i;
+      sleep(1);
+      printf("CHILD%d\n", mynum);
+    }
+  } 
+  // is parent process running?
+  else if (childPID > 0) //Parent process
+  {
+    printf("Parent process ID: %d\n", getpid());
+    for (i = 0;; ++i) /*both processes do this*/ {
+      sleep(1);
+      printf("PARENT%d\n", mynum);
+    }
+  } else // fork failed
+  {
+    printf("\n Fork failed, quitting!!!!!!\n");
+    return 1;
+  }
+  return 0;
+}
---
+++ cFiles/exe2_2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+#define NUM_THREADS 4
+
+void *hello(void * );
+
+int main() {
+  int j;
+  pthread_t tid[NUM_THREADS];
+  printf("My process ID %d\n", getpid());
+  for (j = 0; j < NUM_THREADS; j++) {
+    //create child process with name = tid[j], run hello function with j parameter.
+    int *myid = malloc(sizeof(int));
+    *myid = j;
+    pthread_create(&tid[j], NULL, hello, (void *)myid);
+  }
+
+  for (int i = 0; i < NUM_THREADS; i++)
+    //main function is done if all child process is done.
+    pthread_join(tid[i], NULL);
+  return 0;
+}
+
+void *hello(void * my_id) {
+  printf("Hello World from branch thread %d\n", *(int * ) my_id);
+}
